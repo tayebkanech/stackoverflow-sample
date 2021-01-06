767a16633ae99504f76d3e9e81229286a81189cd
james.lee@ncoresoft.net <james.lee@ncoresoft.net>
2021-01-06 11:16:41 +0900

    Update

--- src/answers/custom-calendar-app/CalendarCore/Models/DayModel.cs
+++ src/answers/custom-calendar-app/CalendarCore/Models/DayModel.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 namespace CalendarCore.Models
 {
-    public class DayModel
-    {
-        public DateTime Date { get; set; }
+  public class DayModel
+  {
+      public DateTime Date { get; set; }
 
-        public int Year => Date.Year;
-        public int Month => Date.Month;
-        public int Day => Date.Day;
+      public int Year => Date.Year;
+      public int Month => Date.Month;
+      public int Day => Date.Day;
 
-        public bool IsLastMonth { get; set; }
-        public bool IsCurrentMonth { get; set; }
-        public bool IsNextMonth { get; set; }
-    }
+      public bool IsLastMonth { get; set; }
+      public bool IsCurrentMonth { get; set; }
+      public bool IsNextMonth { get; set; }
+  }
 }
