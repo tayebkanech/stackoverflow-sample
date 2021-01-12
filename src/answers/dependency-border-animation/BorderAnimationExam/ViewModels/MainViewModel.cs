a277d8d5796e46a0c4eac446c284d413e1946a6e
james.lee@ncoresoft.net <james.lee@ncoresoft.net>
2021-01-12 09:33:15 +0900

    Update: BorderAnimationExam

---
+++ src/answers/dependency-border-animation/BorderAnimationExam/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using BorderAnimationExam.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BorderAnimationExam.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private bool _enable_ctl;
+        public bool Enable_control
+        {
+            get { return _enable_ctl; }
+            set { _enable_ctl = value; OnPropertyChanged(); }
+        }
+        
+
+        public MainViewModel()
+        {
+        
+        }
+    }
+}
