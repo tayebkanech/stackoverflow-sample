987ca231dd0bf61298db9aa48308618c5a004e7e
james.lee@ncoresoft.net <james.lee@ncoresoft.net>
2021-01-18 18:32:04 +0900

    Update

---
+++ src/answers/quiz-app/WpfQuizApp/WpfQuizApp/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfQuizApp.Data;
+using WpfQuizApp.Units;
+
+namespace WpfQuizApp
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitBindData();
+        }
+
+        private void InitBindData()
+        {
+            var source = new List<ExampleModel>();
+            source.Add(new ExampleModel { Sentence = "Hello @my friends." });
+
+            foreach (var item in source)
+            {
+                var examCtrl = new ExampleControl();
+                exams.Children.Add(examCtrl);
+
+            }
+        }
+    }
+}
---
+++ src/answers/quiz-app/WpfQuizApp/WpfQuizApp/Units/ExampleControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfQuizApp.Units
+{
+    public class ExampleControl : Control
+    {
+        public static readonly DependencyProperty SentenceProperty = DependencyProperty.Register(
+            "Sentence", typeof(string), typeof(ExampleControl), new PropertyMetadata(""));
+
+        public string Header
+        {
+            get { return (string)this.GetValue(SentenceProperty); }
+            set { this.SetValue(SentenceProperty, value); }
+        }
+    }
+}
