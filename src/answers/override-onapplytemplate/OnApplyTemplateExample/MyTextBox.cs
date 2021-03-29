46d2bbb78a3dbaddf01ccf5781936cc037603d9b
james.lee <james.lee@devncore.org>
2021-03-29 20:40:50 +0900

    add onapplytemplate

---
+++ src/answers/override-onapplytemplate/OnApplyTemplateExample/MyTextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace OnApplyTemplateExample
+{
+    public class MyTextBox : TextBox
+    {
+        public MyTextBox()
+        { 
+        
+        }
+
+        public override void OnApplyTemplate()
+        {
+            var border = this.Template.FindName("border", this) as Border;
+            border.Background = Brushes.Red;
+        }
+    }
+}
