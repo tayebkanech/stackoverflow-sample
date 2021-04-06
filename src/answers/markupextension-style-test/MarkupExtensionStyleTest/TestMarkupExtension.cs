f7755843d04d896fa0f44f0ec51c8864b76b66d9
james.lee <james.lee@devncore.org>
2021-04-06 19:18:37 +0900

    Add Markupextension style test

---
+++ src/answers/markupextension-style-test/MarkupExtensionStyleTest/TestMarkupExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MarkupExtensionStyleTest
+{
+    public class TestMarkup : MarkupExtension
+    {
+        public string Extension { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var target = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+            if (target.TargetObject is Button ctl)
+            {
+
+            }
+            return Extension;
+        }
+    }
+}
