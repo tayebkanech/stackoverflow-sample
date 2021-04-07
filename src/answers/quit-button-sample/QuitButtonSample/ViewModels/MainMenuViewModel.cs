9ad5946d192ce45d5d701810fc99eba14ec8211b
Yejin Kim <elena.kim@devncore.org>
2021-04-07 21:29:48 +0900

    Update Source

--- src/answers/quit-button-sample/QuitButtonSample/ViewModels/MainMenuViewModel.cs
+++ src/answers/quit-button-sample/QuitButtonSample/ViewModels/MainMenuViewModel.cs
@@ -1,30 +1,18 @@
 ﻿using QuitButtonSample.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace QuitButtonSample.ViewModels
 {
     public class MainMenuViewModel
     {
-        #region .ICommand.
         public ICommand QuitCommand { get; set; }
-        #endregion
 
-        #region .Constructor.
         public MainMenuViewModel()
         { 
             QuitCommand = new RelayCommand<object>(QuitApp);
         }
 
-        #endregion
-
-        #region .Method.
         private void QuitApp(object obj)
         {
             if (obj is Window window)
@@ -32,6 +20,5 @@ namespace QuitButtonSample.ViewModels
                 window.Close();
             }
         }
-        #endregion
     }
 }
