10ed7f9a8ea4ffc2d19c37f99bbd9874e6e1884c
ljw <james.lee@devncore.org>
2021-04-13 13:33:27 +0900

    Update

---
+++ src/answers/listboxitem-animation-example/ListBoxItemAnimationExample/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ListBoxItemAnimationExample
+{
+	internal class MainViewModel
+	{
+		private List<UserModel> _users;
+
+		public List<UserModel> Users
+		{ 
+			get { return _users; }
+			set { _users = value; }
+		}
+
+		public MainViewModel()
+		{
+			Users = new List<UserModel>
+			{
+				new UserModel { Header = "Elena", SubItem = "Seoul" },
+				new UserModel { Header = "James", SubItem = "Seoul" }
+			};
+		}
+	}
+}
---
+++ src/answers/listboxitem-animation-example/ListBoxItemAnimationExample/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace ListBoxItemAnimationExample
+{
+	internal class UserModel
+	{
+		public string Header { get; internal set; }
+		public string SubItem { get; internal set; }
+	}
+}
