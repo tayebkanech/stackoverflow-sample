892417eabeddf9b3813792cf045c6c4e0ad74f17
ljw <james.lee@devncore.org>
2021-04-16 11:55:46 +0900

    Update

---
+++ src/answers/relativesource-test-app/ViewModels/FirstViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RelativeSourceTestApp.ViewModels
+{
+	public class FirstViewModel
+	{
+		public SolidColorBrush TabControlBackground { get; set; }
+
+		public FirstViewModel()
+		{
+			TabControlBackground = Brushes.Gray;
+		}
+	}
+}
---
+++ src/answers/relativesource-test-app/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RelativeSourceTestApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace RelativeSourceTestApp.ViewModels
+{
+	public class MainViewModel : ObservableObject
+	{
+		public FirstViewModel G1VM { get; set; }
+		public SecondViewModel G2VM { get; set; }
+
+		public SolidColorBrush GridBackground { get; set; }
+
+		public MainViewModel()
+		{
+			G1VM = new FirstViewModel();
+			G2VM = new SecondViewModel();
+
+			GridBackground = Brushes.AliceBlue;
+		}
+	}
+}
