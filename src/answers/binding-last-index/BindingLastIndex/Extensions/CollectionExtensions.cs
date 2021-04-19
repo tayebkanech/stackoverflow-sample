5105aa299505e70c130889eaadc80d1275475c5a
ljw <james.lee@devncore.org>
2021-04-19 09:51:39 +0900

    Update

--- src/answers/binding-last-index/BindingLastIndex/Extensions/CollectionExtensions.cs
+++ src/answers/binding-last-index/BindingLastIndex/Extensions/CollectionExtensions.cs
@@ -7,34 +7,34 @@ using System.Runtime.CompilerServices;
 
 namespace BindingLastIndex
 {
-	public class UserItems : ObservableCollection<UserItem>, INotifyPropertyChanged
+public class UserItems : ObservableCollection<UserItem>, INotifyPropertyChanged
+{
+	private UserItem _firstItem;
+	private UserItem _userItem;
+
+	public UserItem FirstUser
 	{
-		private UserItem _firstItem;
-		private UserItem _userItem;
-
-		public UserItem FirstUser
-		{
-			get { return _firstItem; }
-			set { _firstItem = value; OnPropertyChanged(); }
-		}
-
-		public UserItem LastUser
-		{
-			get { return _userItem; }
-			set { _userItem = value; OnPropertyChanged(); }
-		}
-
-		protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
-		{
-			base.OnCollectionChanged(e);
-			FirstUser = this.FirstOrDefault();
-			LastUser = this.LastOrDefault();
-		}
-
-		public new event PropertyChangedEventHandler PropertyChanged;
-		protected void OnPropertyChanged([CallerMemberName] string name = null)
-		{
-			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-		}
+		get { return _firstItem; }
+		set { _firstItem = value; OnPropertyChanged(); }
 	}
+
+	public UserItem LastUser
+	{
+		get { return _userItem; }
+		set { _userItem = value; OnPropertyChanged(); }
+	}
+
+	protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+	{
+		base.OnCollectionChanged(e);
+		FirstUser = this.FirstOrDefault();
+		LastUser = this.LastOrDefault();
+	}
+
+	public new event PropertyChangedEventHandler PropertyChanged;
+	protected void OnPropertyChanged([CallerMemberName] string name = null)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
 }
