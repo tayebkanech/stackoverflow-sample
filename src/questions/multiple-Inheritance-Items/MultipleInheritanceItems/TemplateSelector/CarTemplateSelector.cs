e3dfe942cbccaebc9ba2e9fb5d24169c43bfb52f
james.lee <james.lee@devncore.org>
2021-04-20 22:35:05 +0900

    Update datatemplateselector

---
+++ src/questions/multiple-Inheritance-Items/MultipleInheritanceItems/TemplateSelector/CarTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿using MultipleInheritanceItems.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MultipleInheritanceItems.TemplateSelector
+{
+    public class CarTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate ElectricTemplate { get; set; }
+        public DataTemplate GasolineTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Electric)
+            {
+                return ElectricTemplate;
+            }
+            else if (item is Gasoline)
+            {
+                return GasolineTemplate;
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
