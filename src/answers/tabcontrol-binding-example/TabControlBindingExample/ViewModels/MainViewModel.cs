43289a8d0b28f9feec5ac424e0e31238169c7903
kyj <elena.kim@devncore.org>
2021-05-04 18:02:10 +0900

    Add Project

---
+++ src/answers/tabcontrol-binding-example/TabControlBindingExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using TabControlBindingExample.Mvvm;
+
+namespace TabControlBindingExample.ViewModels
+{
+	public class MainViewModel
+	{
+		private TabsWindow win;
+
+		public ICommand BtnClick { get; set; }
+
+		public MainViewModel()
+		{
+			BtnClick = new RelayCommand<object>(Click);
+		}
+
+		private void Click(object obj)
+		{
+			if (obj is Button btn)
+			{
+				if (win is null || !win.IsVisible)
+				{ 
+					win = new TabsWindow();
+				}
+				win.SetTab(btn.Content.ToString());
+
+			}
+		}
+	}
+}
---
+++ src/answers/tabcontrol-binding-example/TabControlBindingExample/Views/ButtonsWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using TabControlBindingExample.ViewModels;
+
+namespace TabControlBindingExample.Views
+{
+	/// <summary>
+	/// ButtonsWindow.xaml에 대한 상호 작용 논리
+	/// </summary>
+	public partial class ButtonsWindow : Window
+	{
+		public ButtonsWindow()
+		{
+			InitializeComponent();
+			DataContext = new MainViewModel();
+		}
+	}
+}
