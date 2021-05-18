fa36a2b943f16399c544b5b8a76608595032876e
james <james.lee@devncore.org>
2021-05-18 10:52:58 +0900

    Update

--- src/questions/contenttemplate-binding/ContentTemplateExample/MainViewModel.cs
+++ src/questions/contenttemplate-binding/ContentTemplateExample/MainViewModel.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ContentTemplateExample
 {
+	public class Test
+	{ 
+		public string Name { get; set; }
+	}
 	public class MainViewModel
 	{
+		public string Name { get; set; } = "Test;";
+
+		public List<Test> Source = new List<Test>();
+		public MainViewModel()
+		{
+			Source.Add(new Test());
+			Source.Add(new Test());
+		}
 	}
 }
